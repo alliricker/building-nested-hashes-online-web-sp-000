e9599a94228cb8923a29e5663c640cbc2d3c928f
Allison Ricker <alliricker@gmail.com>
2020-09-28 21:14:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
-      :hero_friends => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       },
     :capulet => {
-      :partriarch => {}
-      :matriarch => {}
-      :heroine => {}
-      :heroine_friends => {}
-      }}
+      :partriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+    }
 
 
 end
